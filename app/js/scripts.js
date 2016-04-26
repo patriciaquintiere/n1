e4d48579ce4cbe5d0ee7121b4a4dcc3762e0e661
patriciaquintiere <patriciaquintiere@gmail.com>
2016-04-26 15:04:19 -0300

    teste pagina git

---
+++ app/js/scripts.js
@@ -0,0 +1,51 @@
+/*
+* session save itens cart
+*/
+// function saveItems(idProduct) {
+//   var saveProducts = '';
+//   saveProducts += ' idProduct=' + idProduct + '/';
+//   document.cookie += saveProducts;
+// }
+
+/*
+* get products
+*/
+function viewProducts(){
+  $.ajax({
+    url: "../data.json",
+    dataType: "json",
+    success: function(products){
+      $(products).each(function(index, product){
+        // html content blocks
+        var contentProduct = '';
+        // init block product
+        contentProduct += '<div class="block product">\n';
+        // product url image
+        contentProduct += ' <img '+'src="' + product.productImagerl + '" alt="Foto '+product.productName+'" />\n';
+        // product info
+        contentProduct += ' <p class="info-product">\n';
+        // product name
+        contentProduct += '   <span class="name">' + product.productName + '</span>';
+        // product especification
+        contentProduct += '   <span class="escecification">' + product.productEspecification + '</span>';
+        // product description
+        contentProduct += ' </p>\n';
+        // product price tag
+        contentProduct += ' <div class="price">\n';
+        // product price
+        var formatProductPrice = formataValor(product.productPrice);
+        contentProduct += ' <span>R$ </span>';
+        contentProduct += ' <span class="current-price">'+formatProductPrice+'</span>';
+        // end block price
+        contentProduct += ' </div>';
+        // end block product
+        contentProduct += '</div>';
+
+        $(".newproducts .container-products").append(contentProduct);
+      });
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown){
+      $(".newproducts .container-products").append('<div class="block">Desculpe, não foi possível localizar os produtos.</p>');
+    }
+  });
+}
