ce794abab59a2a747c82da4895b1c3aa11c78886
patriciaquintiere <patriciaquintiere@gmail.com>
2016-04-27 00:16:44 -0300

    responsive layout

--- app/js/home.js
+++ app/js/home.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
     speed: 900,
     autoplay: true
   });
-  var value = window.devicePixelRatio;
-  var w = window.innerWidth;
-  var lWidth = window.screen.width;
-  console.log(value);
-  console.log(w);
-  console.log(lWidth);
+  // var value = window.devicePixelRatio;
+  // var w = window.innerWidth;
+  // var lWidth = window.screen.width;
+  // console.log(value);
+  // console.log(w);
+  // console.log(lWidth);
 
 });
